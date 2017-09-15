21ec9cbbb47559cf960d25576824e40fc96d5895
EnthusiastDW <dw569490941@gmail.com>
2017-09-15 10:50:16 +0800

    增加类别和IP地址

--- document/lists.js
+++ document/lists.js
@@ -1,14 +1,22 @@
 var lists = [{
 		title:"修改shift右键命令窗口为CMD",
+		cate:"tips",
 		url:"修改shift右键命令窗口为CMD.txt"
 	},{
 		title:"安装Linux虚拟机教程",
+		cate:"ducuments",
 		url:"安装Linux虚拟机教程.docx"
 	},{
 		title:"Spring boot简单搭建",
+		cate:"ducuments",
 		url:"Spring boot搭建.docx"
 	},{
 		title:"Springboot问题和注解",
+		cate:"tips",
 		url:"Springboot问题和注解.md"
-	},
+	},{
+		title:"windows端口占用和解除",
+		cate:"tips",
+		url:"windows端口占用和解除.txt"
+	}
 ];
